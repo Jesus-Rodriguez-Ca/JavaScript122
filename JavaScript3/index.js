2127b23c723ef4f42d4fe9d6796b71b33e842037
Jesus Rodriguez <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2021-05-08 20:07:19 -0700

    Add files via upload

--- JavaScript3/index.js
+++ JavaScript3/index.js
@@ -1,18 +1,17 @@
+const dataModule = require("./data");
 const express = require("express");
 const exphbs = require("express-handlebars");
+
 const app = express();
 const port = 3000;
-const fs = require("fs");
 
-let data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
-
-let games = data['games'];
+let allGames = dataModule.getAll();
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-app.get("/" , function (req, res) {
-  res.render("home", { games:games });
+app.get("/", (req, res) => {
+  res.render("home", { games: allGames, addItem: dataModule.addItem });
 });
 
 app.get("/about", (req, res) => {
@@ -20,11 +19,9 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/detail", (req, res) => {
-  let item = req.query.item;
-  console.log("number" + item);
-  let temp = games.filter(word => word.name === item);
-  console.log(temp);
-  res.render("detail", { game: temp[0]});
+  const { item } = req.query;
+  const temp = dataModule.getItem(item);
+  res.render("detail", { game: temp[0] });
 });
 
 app.listen(port, () => {
