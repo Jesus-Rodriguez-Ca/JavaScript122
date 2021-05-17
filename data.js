3ff09d8e9c6f67dcb49b50c79d456b7cac909d00
Jesus Rodriguez <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2021-05-16 22:16:33 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,122 @@
+require("./models/game");
+
+function game(id, name, price, version, available) {
+  this.id = id;
+  this.name = name;
+  this.price = price;
+  this.version = version;
+  this.available = available;
+}
+
+let games = [
+  {
+    id: 1,
+    name: "Smash Bros",
+    price: "60",
+    version: "1.2.1",
+    available: true,
+  },
+  {
+    id: 2,
+    name: "Zelda",
+    price: "70",
+    version: "2.2.2",
+    available: false,
+  },
+  {
+    id: 3,
+    name: "Super Mario",
+    price: "65",
+    version: "2.1",
+    available: true,
+  },
+  {
+    id: 4,
+    name: "Pokemon",
+    price: "68",
+    version: "1.2.3",
+    available: true,
+  },
+  {
+    id: 5,
+    name: "Splatoon",
+    price: "71",
+    version: "2.2.3",
+    available: false,
+  },
+];
+
+function getItem(game) {
+  // Gets an element by name from the array games
+  return games.filter((word) => word.name === game);
+}
+
+function getAll() {
+  // Gets all the elements of the array games
+  return games;
+}
+
+function addItem(tempGame) {
+  let newGame = new game(
+    tempGame.id,
+    tempGame.name,
+    tempGame.price,
+    tempGame.version,
+    tempGame.available
+  );
+  let temp = getItem(tempGame.name);
+  if (temp.length == 0) {
+    games.push(newGame);
+    console.log(games);
+    console.log("New Game added to the list!");
+  } else {
+    console.log("Game already exist!");
+  }
+}
+
+addItem({id:1, name:"Perfect Dark", price: 50.00,version: 1.3,available: true})
+
+function deleteItem(game) {
+  let myArr = games;
+  let index = myArr.findIndex(function (o) {
+    return o.name === game;
+  });
+  if (index !== -1) myArr.splice(index, 1);
+  //return myArr.splice(index, 1);
+  console.log(myArr);
+}
+console.log(games.length);
+
+// return all records
+Game.find({})
+  .lean()
+  .then((games) => {
+    console.log(games);
+  })
+  .catch((err) => next(err));
+
+// return all records that match a condition
+Game.find({ name: "Zelda" })
+  .lean()
+  .then((games) => {
+    console.log(games);
+  })
+  .catch((err) => next(err));
+
+// return a single record
+Game.findOne({ name: "Zelda" })
+  .lean()
+  .then((game) => {
+    console.log(game);
+  })
+  .catch((err) => next(err));
+
+// // insert or update a single record
+// const newGame = {'id': '7', 'name':'Luigi', 'price':71, 'version': '2.3.1', 'available': true }
+// Game.update({'name':'Zelda'}, newGame, {upsert:true}, (err, result) => {
+//   if (err) return next(err);
+//   console.log(result);
+//   // other code here
+// });
+
+module.exports = { getAll, getItem, addItem, deleteItem, games, game };
