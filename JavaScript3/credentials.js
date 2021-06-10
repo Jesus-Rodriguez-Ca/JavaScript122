8c1b408026a1a0d06e775082d9f41cd36f8a91ad
Jesus Rodriguez <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2021-06-10 09:47:18 -0700

    Add files via upload

---
+++ JavaScript3/credentials.js
@@ -0,0 +1,4 @@
+module.exports ={ const :connection = "mongodb+srv://JesusRodriguez:Snet123!@cluster0.sy9op.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"};
+
+// NOTES_APP_MONGODB_HOST =localhost;
+// NOTES_APP_MONGODB_DATABASE =myFirstDatabase;
